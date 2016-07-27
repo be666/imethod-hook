de2b55555d9b1c4856f0738a7477258affefbea4
bqxu <bcaring@163.com>
2016-07-27 21:35:14 +0800

    Branch hook-web

--- server/boot/root.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-module.exports = function(server) {
-  // Install a `/` route that returns server status
-};
--- server/boot/routes.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-var path = require('path');
-module.exports = function (app) {
-  app.get('/state', app.loopback.status());
-};
